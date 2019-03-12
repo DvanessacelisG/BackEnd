57d6575e0126cfb32ab747dd6dcc28d73fe4b499
DvanessacelisG <46729805+DvanessacelisG@users.noreply.github.com>
2019-03-12 14:22:54 -0500

    Update seeds.js

--- seeds.js
+++ seeds.js
@@ -11,6 +11,29 @@ async function main () {
       database: process.env.DB_NAME || 'Vane_RDS'
     })
     pool.query = util.promisify(pool.query)
+   const publicationsQuery = 'INSERT INTO publications (name, avatar) VALUES ?'
+    const publicationsValues = [
+      ['The Daily Reviewer', 'glyphicon-eye-open'],
+      ['International Movie Critic', 'glyphicon-fire'],
+      ['MoviesNow', 'glyphicon-time'],
+      ['MyNextReview', 'glyphicon-record'],
+      ['Movies n\' Games', 'glyphicon-heart-empty'],
+      ['TheOne', 'glyphicon-globe'],
+      ['ComicBookHero.com', 'glyphicon-flash']
+    ]
+    await pool.query(publicationsQuery, [publicationsValues])
+
+    const reviewersQuery = 'INSERT INTO reviewers (name, publication, avatar) VALUES ?'
+    const reviewersValues = [
+      ['Robert Smith', 'The Daily Reviewer', 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg'],
+      ['Chris Harris', 'International Movie Critic', 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg'],
+      ['Janet Garcia', 'MoviesNow', 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg'],
+      ['Andrew West', 'MyNextReview', 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg'],
+      ['Mindy Lee', 'Movies n\' Games', 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg'],
+      ['Martin Thomas', 'TheOne', 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg'],
+      ['Anthony Miller', 'ComicBookHero.com', 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg']
+    ]
+    await pool.query(reviewersQuery, [reviewersValues])
 
     const moviesQuery = 'INSERT INTO movies (title, release_year, score, reviewer, publication) VALUES ?'
     const moviesValues = [
